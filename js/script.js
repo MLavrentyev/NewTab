97252056879328238e2ef86e9189c8a1fdd4a32b
Mark Lavrentyev <lavrema@outlook.com>
2017-01-23 21:02:15 -0500

    Add time to center block

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ function readConfigFile() {
 	}
 	let finalCell = row.insertCell(i);
 }
+function setTime() {
+	var date = new Date();
+	
+	if(date.getMinutes() < 10) {
+		document.getElementById("time").innerHTML = date.getHours() + ":0" + date.getMinutes();
+	} else {
+		document.getElementById("time").innerHTML = date.getHours() + ":" + date.getMinutes();
+	}
+	setTimeout(setTime(), 60000);
+}
+
+function startScript() {
+	readConfigFile();
+	setTime();
+}
